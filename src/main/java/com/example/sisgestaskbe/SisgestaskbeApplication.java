51832374437214d1d90b477a8a2ea204f844cc63
Carlos E. Pedroza Noguera <carlospedroza21@gmail.com>
2025-02-14 17:04:26 -0500

    Creacion de proyecto

---
+++ src/main/java/com/example/sisgestaskbe/SisgestaskbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sisgestaskbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgestaskbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgestaskbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sisgestaskbe/SisgestaskbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sisgestaskbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgestaskbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
